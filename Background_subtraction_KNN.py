4adbc8f529973ec6610250d7542ea58f8dcd6d28
Rúben Santos <rubenfjs.94@gmail.com>
2022-01-06 22:08:29 +0000

    Tracking com bounding boxes

--- Background_subtraction_KNN.py
+++ Background_subtraction_KNN.py
@@ -1,58 +1,113 @@
 import cv2
+import imutils
 
-cap = cv2.VideoCapture('video_files/GH010347.MP4')
-#cap = cv2.VideoCapture('E:/Pictures & Videos/Videos/GoPro/GH010347.MP4')
-#cap = cv2.VideoCapture('video_files/PXL_20210522_093608367.mp4')
-cap.set(cv2.CAP_PROP_BUFFERSIZE, 40)
-#cap2.set(cv2.CAP_PROP_BUFFERSIZE, 40)
+from camera_calibration import CameraCalibration
 
-success, img = cap.read()
+class BackgroundSubtractionKNN:
 
-myvideo=cv2.VideoWriter("video_files/forgroundKNN.avi", cv2.VideoWriter_fourcc('M','J','P','G'), 30, (int(img.shape[1]),int(img.shape[0])))
+    outputFrame = None
 
-BS_KNN = cv2.createBackgroundSubtractorKNN()
+    def __init__(self, video_name):
+        self.video_name = video_name
 
-#tracker = cv2.TrackerMOSSE_create()
-while cap.isOpened():
-    #timer = cv2.getTickCount()
-    success, img = cap.read()
-    #success2, img2 = cap2.read()
+    def subtractor(self, lock, area):
+        camera_calibration = CameraCalibration(self.video_name)
+        camera_calibration.calibrate(lock)
+        cap = cv2.VideoCapture(self.video_name)
+        #cap = cv2.VideoCapture('E:/Pictures & Videos/Videos/GoPro/GH010347.MP4')
+        #cap = cv2.VideoCapture('video_files/PXL_20210522_093608367.mp4')
+        cap.set(cv2.CAP_PROP_BUFFERSIZE, 40)
+        #cap2.set(cv2.CAP_PROP_BUFFERSIZE, 40)
 
-    #fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
-    #cv2.putText(img, str(int(cap.get(cv2.CAP_PROP_FPS))), (75, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-    #cv2.putText(img2, str(int(cap2.get(cv2.CAP_PROP_FPS))), (75, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        success, img = cap.read()
 
-    cv2.namedWindow("Pier cam", cv2.WINDOW_NORMAL)  # Create window with freedom of dimensions
+        #myvideo=cv2.VideoWriter("video_files/forgroundKNN.avi", cv2.VideoWriter_fourcc('M','J','P','G'), 30, (int(img.shape[1]),int(img.shape[0])))
 
-    imS = cv2.resize(img, (960, 540))  # Resize image
-    #imS2 = cv2.resize(img2, (960, 540))  # Resize image
+        BS_KNN = cv2.createBackgroundSubtractorKNN()
+        #BS_KNN = cv2.createBackgroundSubtractorMOG2()
 
-    cv2.rectangle(imS, (10, 2), (100,20), (255,255,255), -1)
-    cv2.putText(imS, str(cap.get(cv2.CAP_PROP_POS_FRAMES)), (15, 15),
-               cv2.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
+        #tracker = cv2.TrackerMOSSE_create()
+        while cap.isOpened():
+            #timer = cv2.getTickCount()
+            success, img = cap.read()
+            #success2, img2 = cap2.read()
 
-    cv2.imshow("Pier cam", imS)
-    #cv2.imshow("Pier cam 2", imS2)
+            #fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
+            #cv2.putText(img, str(int(cap.get(cv2.CAP_PROP_FPS))), (75, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+            #cv2.putText(img2, str(int(cap2.get(cv2.CAP_PROP_FPS))), (75, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
-
-
-    fgKnn = BS_KNN.apply(img)
-
-    fg = cv2.copyTo(img, fgKnn)
-    myvideo.write(fg)
-
-    fgKnnRs = cv2.resize(fgKnn, (960, 540))  # Resize image
-
-    cv2.rectangle(fgKnnRs, (10, 2), (140, 20), (255, 255, 255), -1)
-    cv2.putText(fgKnnRs, str(cap.get(cv2.CAP_PROP_POS_FRAMES)), (15, 15),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
-    cv2.putText(fgKnnRs, str(int(cap.get(cv2.CAP_PROP_FPS))), (75, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
+            if img is not None:
 
 
-    cv2.imshow("Foreground KNN", fgKnnRs)
+                cv2.namedWindow("Pier cam", cv2.WINDOW_NORMAL)  # Create window with freedom of dimensions
 
-    if cv2.waitKey(1) & 0xff == ord('q'):
-        break
+                imS = cv2.resize(img, (960, 540))  # Resize image
+                #imS2 = cv2.resize(img2, (960, 540))  # Resize image
 
-cv2.destroyAllWindows()
-cap.release()
+                cv2.rectangle(imS, (10, 2), (100,20), (255,255,255), -1)
+                cv2.putText(imS, str(cap.get(cv2.CAP_PROP_POS_FRAMES)), (15, 15),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
+
+                cv2.imshow("Pier cam", imS)
+                #cv2.imshow("Pier cam 2", imS2)
+
+                cv2.namedWindow("Pier cam undistorted", cv2.WINDOW_NORMAL)
+                undistorted_img = camera_calibration.undistort(img)
+                if undistorted_img is not None:
+                    cv2.imshow("Pier cam undistorted", undistorted_img)
+                    fgKnn = BS_KNN.apply(undistorted_img)
+
+                #fgKnn = BS_KNN.apply(img)
+
+
+                #fg = cv2.copyTo(img, fgKnn)
+                #myvideo.write(fg)
+
+                fgKnnRs = self.select_objects(area, cap, fgKnn)
+
+            if cv2.waitKey(1) & 0xff == ord('q'):
+                break
+
+
+        cv2.destroyAllWindows()
+        cap.release()
+
+    def select_objects(self, area, cap, fgKnn):
+        if fgKnn is not None:
+            fgKnnRs = cv2.resize(fgKnn, (960, 540))  # Resize image
+
+            cv2.rectangle(fgKnnRs, (10, 2), (140, 20), (255, 255, 255), -1)
+            cv2.putText(fgKnnRs, str(cap.get(cv2.CAP_PROP_POS_FRAMES)), (15, 15),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
+            cv2.putText(fgKnnRs, str(int(cap.get(cv2.CAP_PROP_FPS))), (75, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                        (0, 0, 0))
+
+            fgKnnRs = self.get_centroid(area, fgKnnRs)
+
+            cv2.imshow("Foreground KNN", fgKnnRs)
+        return fgKnnRs
+
+    def get_centroid(self, area, fgKnnRs):
+        (contours, hierarchy) = cv2.findContours(fgKnnRs.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        for c in contours:
+            if cv2.contourArea(c) < area:
+                continue
+
+            # get bounding box from countour
+            (x, y, w, h) = cv2.boundingRect(c)
+
+            # draw bounding box
+            cv2.rectangle(fgKnnRs, (x, y), (x + w, y + h), (100, 0, 255), 2)
+
+            moments = cv2.moments(c)
+            cX = int(moments["m10"] / moments["m00"])
+            cY = int(moments["m01"] / moments["m00"])
+            # draw the contour and center of the shape on the image
+            cv2.drawContours(fgKnnRs, [c], -1, (0, 255, 0), 2)
+            cv2.circle(fgKnnRs, (cX, cY), 7, (255, 255, 255), -1)
+            print(cX)
+            print(cY)
+            cv2.putText(fgKnnRs, "center", (cX - 20, cY - 20),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+
+        return fgKnnRs
---
+++ server.py
@@ -0,0 +1,96 @@
+import cv2
+import tkinter as tk
+from imutils.video import VideoStream
+from flask import Response
+from flask import Flask
+from flask import render_template
+import threading
+
+from Background_subtraction_KNN import BackgroundSubtractionKNN
+
+
+outputFrame = None
+bs1 = None
+bs2 = None
+cap1 = None
+cap2 = None
+lock = threading.Lock()
+app = Flask(__name__)
+
+@app.route("/")
+def web_page():
+	# return the rendered template
+	return render_template("impactDetection.html")
+
+@app.route("/video_feed_1")
+def video_feed_1():
+	# return the response generated along with the specific media
+	# type (mime type)
+	global bs1
+	return Response(generate(bs1),
+		mimetype = "multipart/x-mixed-replace; boundary=frame")
+
+@app.route("/video_feed_2")
+def video_feed_2():
+	# return the response generated along with the specific media
+	# type (mime type)
+	global bs2
+	return Response(generate(bs2),
+		mimetype = "multipart/x-mixed-replace; boundary=frame")
+
+@app.route("/video_feed_test")
+def video_feed_test():
+	# return the response generated along with the specific media
+	# type (mime type)
+	global bs2
+	return Response(gen_frames(),
+		mimetype = "multipart/x-mixed-replace; boundary=frame")
+
+def generate(bs):
+	# grab global references to the output frame and lock variables
+	global outputFrame, lock
+	# loop over frames from the output stream
+	while True:
+		# wait until the lock is acquired
+		with lock:
+			if bs is None:
+				continue
+
+			outputFrame = bs.frame()
+			# check if the output frame is available, otherwise skip
+			# the iteration of the loop
+			if outputFrame is None:
+				continue
+			# encode the frame in JPEG format
+			(flag, encodedImage) = cv2.imencode(".jpg", outputFrame)
+			# ensure the frame was successfully encoded
+			if not flag:
+				continue
+		# yield the output frame in the byte format
+		yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
+			bytearray(encodedImage) + b'\r\n')
+
+def gen_frames():
+	global cap1
+
+	while True:
+		if cap1 is None:
+			continue
+		success, frame_test = cap1.read()  # read the camera frame
+		if not success:
+			break
+		else:
+			resized_frame = cv2.resize(frame_test, (960, 540))
+			ret, buffer = cv2.imencode('.jpg', resized_frame)
+			resized_frame = buffer.tobytes()
+			yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + resized_frame + b'\r\n')
+
+# check to see if this is the main thread of execution
+if __name__ == '__main__':
+	#app.run(host='0.0.0.0', port=8000, debug=True)
+	#cap1 = cv2.VideoCapture('video_files/GH010731.MP4')
+	#cap2 = cv2.VideoCapture('video_files/MVI_2438.MP4')
+	bs1 = BackgroundSubtractionKNN('video_files/GH010731_cut.MP4')
+	bs1.subtractor(lock, 5000)
+	bs2 = BackgroundSubtractionKNN('video_files/MVI_2438.MP4')
+	bs2.subtractor(lock, 5000)
