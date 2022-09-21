3cf1c015b046456c16092d7e3fd65fdb6b1a30c0
rubenfjs <rubenfjs.94@gmail.com>
2022-09-21 18:24:41 +0100

    Read from picture list

--- tools/Background_subtraction_KNN.py
+++ tools/Background_subtraction_KNN.py
@@ -1,11 +1,10 @@
+import glob
 import os
 import sys
 
 import cv2
 
 import csv
-
-from cv2 import VideoCapture
 
 from tools.camera_calibration import CameraCalibration
 from external_libraries.centroidTracker import CentroidTracker
@@ -21,12 +20,16 @@ class BackgroundSubtractionKNN:
 
         if os_name == "Windows":
             self.video_name = parent_path + '\\video_files\\' + source_name + '.MP4'
-            self.video_undistorted_name = parent_path + '\\video_files\\' + source_name + '-undistorted.MP4'
+            self.video_undistorted_path = parent_path + '\\video_files\\converted\\' + source_name + '\\'
+            self.video_undistorted_name = self.video_undistorted_path + '*.png'
+            self.fps_file = self.video_undistorted_path + 'fps.txt'
             self.screenshot_name = parent_path + '\\screenshot_files\\' + source_name
             self.centroid_file = parent_path + '\\results\\' + source_name + '-centroids.csv'
         else:
             self.video_name = parent_path + '/video_files/' + source_name + '.MP4'
-            self.video_undistorted_name = parent_path + '/video_files/' + source_name + '-undistorted.MP4'
+            self.video_undistorted_path = parent_path + '/video_files/converted/' + source_name + '/'
+            self.video_undistorted_name = self.video_undistorted_path + '*.png'
+            self.fps_file = self.video_undistorted_path + 'fps.txt'
             self.screenshot_name = parent_path + '/screenshot_files/' + source_name
             self.centroid_file = parent_path + '/results/' + source_name + '-centroids.csv'
 
@@ -94,72 +97,82 @@ class BackgroundSubtractionKNN:
         cap.release()
 
     def subtractor(self, is_test: bool):
+        error = None
 
         history, detect_shadows, dist_2_threshold, centroid_object_min_area = self.get_bs_param(self.source_name)
-        cap = cv2.VideoCapture(self.video_name)
 
-        cap.set(cv2.CAP_PROP_BUFFERSIZE, 40)
+        images = sorted(glob.glob(self.video_undistorted_name), key=os.path.basename)
+
+        if not os.path.exists(self.video_undistorted_path):
+            return "Video hasn't been converted yet"
+
+        if not os.path.exists(self.fps_file):
+            return "Video hasn't been converted yet"
+
+        f = open(self.fps_file, "r")
+        fps = float(f.read())
 
         bs_knn = cv2.createBackgroundSubtractorKNN(history=history, detectShadows=detect_shadows,
                                                    dist2Threshold=dist_2_threshold)
 
-        # tracker = cv2.TrackerMOSSE_create()
-        while cap.isOpened():
-            # timer = cv2.getTickCount()
-            success, img = cap.read()
-            # success2, img2 = cap2.read()
+        if images is not None:
+            for fname in images:
+                img = cv2.imread(fname)
 
-            # fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
-            # cv2.putText(img, str(int(cap.get(cv2.CAP_PROP_FPS))), (75, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-            # cv2.putText(img2, str(int(cap2.get(cv2.CAP_PROP_FPS))), (75, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                frame = int(fname[-5:-4]) + 1
+                # success2, img2 = cap2.read()
 
-            if img is not None:
-                # cv2.namedWindow("Pier cam", cv2.WINDOW_NORMAL)  # Create window with freedom of dimensions
+                # fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
+                # cv2.putText(img, str(int(cap.get(cv2.CAP_PROP_FPS))), (75, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                # cv2.putText(img2, str(int(cap2.get(cv2.CAP_PROP_FPS))), (75, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
-                imS = cv2.resize(img, self.window_size)  # Resize image
-                # imS2 = cv2.resize(img2, (960, 540))  # Resize image
+                if img is not None:
+                    # cv2.namedWindow("Pier cam", cv2.WINDOW_NORMAL)  # Create window with freedom of dimensions
 
-                undistorted_img = self.camera_calibration.undistort(img)
+                    imS = cv2.resize(img, self.window_size)  # Resize image
+                    # imS2 = cv2.resize(img2, (960, 540))  # Resize image
 
-                height = img.shape[0]
-                width = img.shape[1]
+                    #undistorted_img = self.camera_calibration.undistort(img)
 
-                undistorted_img_resized = cv2.resize(undistorted_img, (width, height))
+                    height = img.shape[0]
+                    width = img.shape[1]
 
-                # undistorted_img = undistorted_img_resized
+                    #undistorted_img_resized = cv2.resize(img, (width, height))
 
-                undistorted_img_s = cv2.resize(undistorted_img, self.window_size)
+                    # undistorted_img = undistorted_img_resized
 
-                cv2.rectangle(imS, (10, 2), (100, 20), (255, 255, 255), -1)
-                cv2.putText(imS, str(cap.get(cv2.CAP_PROP_POS_FRAMES)), (15, 15),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
+                    #undistorted_img_s = cv2.resize(img, self.window_size)
 
-                cv2.imshow("Pier cam", imS)
-
-                if undistorted_img is not None:
-                    fgKnn = bs_knn.apply(undistorted_img)
-
-                    cv2.namedWindow("Pier cam undistorted", cv2.WINDOW_NORMAL)
-                    cv2.rectangle(undistorted_img_s, (10, 2), (100, 20), (255, 255, 255), -1)
-                    cv2.putText(undistorted_img_s, str(cap.get(cv2.CAP_PROP_POS_FRAMES)), (15, 15),
+                    cv2.rectangle(imS, (10, 2), (100, 20), (255, 255, 255), -1)
+                    cv2.putText(imS, str(fname), (15, 15),
                                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
-                    cv2.namedWindow("Pier cam undistorted", cv2.WINDOW_NORMAL)
-                    undistorted_img_s2 = cv2.resize(undistorted_img, self.window_size)
-                    cv2.imshow("Pier cam undistorted", undistorted_img_s2)
 
-                    # fgKnn = bs_knn.apply(undistorted_img)
+                    cv2.imshow("Pier cam", imS)
 
-                    self.select_objects(centroid_object_min_area, cap, fgKnn, history, is_test)
+                    #if undistorted_img is not None:
+                    fgKnn = bs_knn.apply(img)
 
-            if cv2.waitKey(1) & 0xff == ord('q'):
-                break
+                        # cv2.namedWindow("Pier cam undistorted", cv2.WINDOW_NORMAL)
+                        # cv2.rectangle(undistorted_img_s, (10, 2), (100, 20), (255, 255, 255), -1)
+                        # cv2.putText(undistorted_img_s, str(fname), (15, 15),
+                        #             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
+                        # cv2.namedWindow("Pier cam undistorted", cv2.WINDOW_NORMAL)
+                        # undistorted_img_s2 = cv2.resize(undistorted_img, self.window_size)
+                        # cv2.imshow("Pier cam undistorted", undistorted_img_s2)
+
+                        # fgKnn = bs_knn.apply(undistorted_img)
+
+                    self.select_objects(centroid_object_min_area, fps, frame, fgKnn, history, is_test)
+
+                if cv2.waitKey(1) & 0xff == ord('q'):
+                    break
 
         cv2.destroyAllWindows()
-        cap.release()
 
-    def select_objects(self, area: int, cap: VideoCapture, fg_knn, history: int, is_test: bool):
+        return error
+    def select_objects(self, area: int, fps: float, frame: int, fg_knn, history: int, is_test: bool):
         if fg_knn is not None:
-            fg_knn_rs = self.get_centroid(area, cap, fg_knn, history, is_test)
+            fg_knn_rs = self.get_centroid(area, fps, frame, fg_knn, history, is_test)
 
             # cv2.namedWindow("Foreground", cv2.WINDOW_NORMAL)
             # cv2.rectangle(fg_knn_rs, (10, 2), (140, 20), (255, 255, 255), -1)
@@ -171,7 +184,7 @@ class BackgroundSubtractionKNN:
             fg_knn_rs2 = cv2.resize(fg_knn_rs, self.window_size)
             cv2.imshow("Foreground", fg_knn_rs2)
 
-    def get_centroid(self, area: int, cap: VideoCapture, fg_knn_rs, history: int, is_test: bool):
+    def get_centroid(self, area: int, fps: float, frame: int, fg_knn_rs, history: int, is_test: bool):
         (contours, hierarchy) = cv2.findContours(fg_knn_rs, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         rects = []
 
@@ -201,7 +214,7 @@ class BackgroundSubtractionKNN:
                 # draw both the ID of the object and the centroid of the
                 # object on the output frame
 
-                if history < cap.get(cv2.CAP_PROP_POS_FRAMES):
+                if history < frame:
                     text = "ID {}".format(objectID)
                     color = (255, 0, 0)
                     cv2.putText(fg_knn_rs, text, (centroid[0] - 10, centroid[1] - 10),
@@ -209,7 +222,7 @@ class BackgroundSubtractionKNN:
                     cv2.circle(fg_knn_rs, (centroid[0], centroid[1]), 4, tuple(color), -1)
 
                     if not is_test:
-                        self.save_centroids(float(cap.get(cv2.CAP_PROP_FPS)), int(cap.get(cv2.CAP_PROP_POS_FRAMES)),
+                        self.save_centroids(fps, frame,
                                             objectID,
                                             centroid[0], centroid[1])
 
