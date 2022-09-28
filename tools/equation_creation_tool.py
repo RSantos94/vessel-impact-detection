c1728e8216b33b60e6887b20812d53787ab2303b
Rúben Santos <rubenfjs.94@gmail.com>
2022-09-28 15:47:32 +0100

    Equation creation tool now suports gimp coordinates

--- tools/equation_creation_tool.py
+++ tools/equation_creation_tool.py
@@ -1,14 +1,29 @@
 if __name__ == '__main__':
-    point_1 = input("Point 1 coordinates: (x1,y2)")
-    point_2 = input("Point 2 coordinates: (x2, y2)")
+    gimp = input("Coordinates from GIMP? (y/n)")
+    if gimp == 'y':
+        resolution = input("Picture resolution: (widthxheight)")
+        resolution_list = resolution.split('x')
+        point_1 = input("Point 1 coordinates: (x1,y2)")
+        point_2 = input("Point 2 coordinates: (x2, y2)")
 
-    point_1_list = point_1.split(',')
-    point_2_list = point_2.split(',')
+        point_1_list = point_1.split(',')
+        point_2_list = point_2.split(',')
 
-    x1 = int(point_1_list[0])
-    y1 = int(point_1_list[1])
-    x2 = int(point_2_list[0])
-    y2 = int(point_2_list[1])
+        x1 = int(point_1_list[0])
+        y1 = abs(int(point_1_list[1]) - int(resolution_list[1]))
+        x2 = int(point_2_list[0])
+        y2 = abs(int(point_2_list[1]) - int(resolution_list[1]))
+    else:
+        point_1 = input("Point 1 coordinates: (x1,y2)")
+        point_2 = input("Point 2 coordinates: (x2, y2)")
+
+        point_1_list = point_1.split(',')
+        point_2_list = point_2.split(',')
+
+        x1 = int(point_1_list[0])
+        y1 = int(point_1_list[1])
+        x2 = int(point_2_list[0])
+        y2 = int(point_2_list[1])
 
     m = (y2 - y1) / (x2 - x1)
 
